07f12f7d709c97b5b663e5f77a566a93995b8708
Nepershin Denis <den.nepershin@gmail.com>
2023-02-02 19:15:59 +0400

    task 2 не полный

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 from mapapi_PG import show_map
 
@@ -8,16 +10,25 @@ SIZE = WIDTH, HEIGHT = 500, 500
 def game():
     running = True
     screen = pygame.display.set_mode(SIZE)
+    ll, span = f'43.141580551356746,51.5570779896406', '0.001,0.001'
+    map_file = show_map(f'll={ll}&spn={span}')
     pygame.display.set_caption('Большая задача по Maps API. Часть №1')
+    screen.blit(pygame.image.load('map.png'), (0, 0))
+    pygame.display.flip()
     while running:
-        ll, span = '43.141580551356746,51.5570779896406', '0.001,0.001'
-        show_map(f'll={ll}&spn={span}')
+        map_file = show_map(f'll={ll}&spn={span}')
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        pygame.render(screen)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_PAGEUP:
+                    print(1)
+                if event.key == pygame.K_PAGEDOWN:
+                    print(2)
+        screen.blit(pygame.image.load('map.png'), (0, 0))
         pygame.display.flip()
     pygame.quit()
+    os.remove(map_file)
 
 
 if __name__ == '__main__':
